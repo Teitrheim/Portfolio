901d726d80b5eb8a12480f0de77c314f3fe845e1
Thomas Eitrheim <teitrheim@gmail.com>
2023-10-10 16:11:55 +0200

    fixing links

--- arrow.js
+++ arrow.js
@@ -13,7 +13,18 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   scrollToTopBtn.addEventListener("click", () => {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
+    scrollToTop();
   });
+
+  function scrollToTop() {
+    const scrollDuration = 500;
+    const scrollStep = -window.scrollY / (scrollDuration / 15);
+    const scrollInterval = setInterval(() => {
+      if (window.scrollY !== 0) {
+        window.scrollBy(0, scrollStep);
+      } else {
+        clearInterval(scrollInterval);
+      }
+    }, 15);
+  }
 });
